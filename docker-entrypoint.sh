6abdf39b7da88d4367e021730616952c61030178
sacundim <anybody@sacundim.org>
2022-03-18 14:19:17 -0700

    Made a mistake in the logic to test whether environment variables existed, had the conditions backwards. Simplify and fix...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 
-set -e -u -x
+set -e -u
 
 PROFILE="${PROFILE-"puerto-rico_profiles/puerto-rico_open/"}"
+S3_DESTINATION="${S3_DESTINATION:?"S3_DESTINATION not set"}"
+DISTRIBUTION_ID="${DISTRIBUTION_ID:?"DISTRIBUTION_ID not set"}"
+
 snakemake --printshellcmds --profile "${PROFILE}"
-
-if [[ -z "${S3_DESTINATION}" ]]
-then
-  aws s3 sync auspice/ "${S3_DESTINATION}"
-else
-  echo "$(date): No S3_DESTINATION set. Skipping aws s3 sync."
-fi
-
-if [[ -z "${DISTRIBUTION_ID}" ]]
-then
-  aws cloudfront create-invalidation \
-    --distribution-id "${DISTRIBUTION_ID}" \
-    --paths '/*'
-else
-  echo "$(date): No DISTRIBUTION_ID set. Skipping CloudFront invalidation."
-fi
+aws s3 sync auspice/ "${S3_DESTINATION}"
+aws cloudfront create-invalidation \
+  --distribution-id "${DISTRIBUTION_ID}" \
+  --paths '/auspice/*'
