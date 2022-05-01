94e964c94c88576c71df1e6f6d6c63f282762430
sacundim <anybody@sacundim.org>
2022-04-30 20:44:00 -0700

    ECS stats say we're using 33% of the memory we've got and 100% of the CPU, so cut memory by half and double the CPUs

--- Terraform/Batch.tf
+++ Terraform/Batch.tf
@@ -22,8 +22,8 @@ resource "aws_batch_job_definition" "nextstrain_job" {
     executionRoleArn = aws_iam_role.ecs_task_role.arn
     jobRoleArn = aws_iam_role.ecs_job_role.arn
     resourceRequirements = [
-      {"type": "VCPU", "value": "4"},
-      {"type": "MEMORY", "value": "14336"}
+      {"type": "VCPU", "value": "8"},
+      {"type": "MEMORY", "value": "6144"}
     ]
     "logConfiguration": {
       "logDriver": "awslogs",
