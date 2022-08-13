380f63065015c428cc78449f5d29ac213d05050b
sacundim <anybody@sacundim.org>
2022-08-12 23:36:54 -0700

    Move the CloudFront configuration out of this repo, since it really belongs now in the covid-19-puerto-rico repo.

--- Terraform/S3.tf
+++ Terraform/S3.tf
@@ -3,11 +3,6 @@ data "aws_s3_bucket" "main_bucket" {
   bucket = var.main_bucket_name
 }
 
-// HTTP access logs bucket
-data "aws_s3_bucket" "logs_bucket" {
-  bucket = var.logs_bucket_name
-}
-
 ###############################################################
 ##
 ## An additional bucket to archive other job run artifacts
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,22 +4,15 @@ variable "project_name" {
   default = "covid-19-puerto-rico-nextstrain"
 }
 
-variable "dns_name" {
+variable "cloudfront_distribution_id" {
   type = string
-  description = "The DNS name of the project website."
-  default = "covid-19-puerto-rico.org"
+  description = "The ID of the CloudFront distribution we publish to."
 }
 
 variable "main_bucket_name" {
   type = string
   description = "The name of the base S3 bucket to create/use."
   default = "covid-19-puerto-rico"
-}
-
-variable "logs_bucket_name" {
-  type = string
-  description = "The name of the bucket to create/use for logs."
-  default = "covid-19-puerto-rico-logs"
 }
 
 variable "jobs_bucket_name" {
